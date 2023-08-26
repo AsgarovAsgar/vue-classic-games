59d7c131d170e2fc0140783afe1673fa1cdeb045
Asgarov Asgar <asgara@maestro.st>
2023-08-26 21:57:23 +0400

    completed dice-game

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ConfettiAnimation from '../components/ConfettiAnimation.vue';
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ConfettiAnimation />
+  </div>
+</template>
